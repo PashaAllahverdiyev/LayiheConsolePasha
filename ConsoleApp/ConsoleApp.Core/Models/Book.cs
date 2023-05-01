ca1b9b1dfe0845d56c351d1c71464c7946ac91fe
PashaAllahverdiyev <pashama@code.edu.az>
2023-05-01 22:56:05 +0400

    ConsoleApp

---
+++ ConsoleApp/ConsoleApp.Core/Models/Book.cs
@@ -0,0 +1,55 @@
+﻿
+
+using ConsoleApp.Core.Enums;
+using ConsoleApp.Core.Models.Base;
+using Microsoft.VisualBasic;
+
+namespace ConsoleApp.Core.Models
+{
+    public class Book:Basemodel
+    {
+        private static int _id;
+        public int NumberOfTheBook { get; set; }
+        public string BookInformation { get; set; }
+        public double Price { get; set; }
+        public double DiscountPrice { get; set; }
+
+        public bool BookInStock { get; set; }
+       
+        public BookCategory Category { get; set; }
+        public BookWriter bookWriter { get; set; }
+        
+
+
+        public Book(string name, int numberofthebook, string information, double price, double discountprice, bool bookInStock, BookCategory category,  BookWriter bookWriter  )
+        {
+            _id++;
+            Id = _id;
+            Name = name;
+            NumberOfTheBook = numberofthebook;
+            BookInformation=information;
+            Price = price;
+            DiscountPrice = discountprice;
+            BookInStock = bookInStock;
+            
+            Category = category;
+            CreatedDate = DateTime.Now;
+            UpdatedDate = DateTime.Now;
+        }
+
+
+        public override string ToString()
+        {
+
+            if (DiscountPrice < Price)
+            {
+                return $"There is  {Price - DiscountPrice} DiscountPrice   Name: {Name}, NumberOfTheBook: {NumberOfTheBook}, BookInformation: {BookInformation}, Price: {DiscountPrice}, BookInStock:{BookInStock},  Category: {Category}, BookWriter: {bookWriter} ";
+            }
+
+
+            return $"Name: {Name}, NumberOfTheBook: {NumberOfTheBook}, BookInformation: {BookInformation}, ,Price: {Price}, BookInStock{BookInStock},  Category: {Category}, BookWriter: {bookWriter} ";
+        }
+    }
+
+}
+
---
+++ ConsoleApp/Services/Services/Implementations/LoginService.cs
@@ -0,0 +1,47 @@
+﻿
+
+namespace ConsoleApp.Services.Services.Implementations
+{
+    public class LoginService
+    {
+        public async Task StartApp()
+        {
+            MenuService consoleAppService = new MenuService();
+
+            Console.WriteLine("1. Admin");
+            Console.WriteLine("2.  User");
+
+            string Request = Console.ReadLine();
+
+            if (Request == "1")
+            {
+                bool result = await consoleAppService.Login();
+
+                while (!result)
+                {
+                    result = await consoleAppService.Login();
+                    if (!result)
+                    {
+
+                        Console.WriteLine("2.Return   User");
+                        Request = Console.ReadLine();
+                        if (Request == "2")
+                        {
+                            result = true;
+                        }
+                    }
+                }
+            }
+
+            if (consoleAppService.IsAdmin)
+            {
+                consoleAppService.ShowAdmin();
+            }
+            else
+            {
+                consoleAppService.ShowUser();
+            }
+
+        }
+    }
+}
